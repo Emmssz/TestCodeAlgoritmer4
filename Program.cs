250d50fd2b231b5a2a6488d473258c0611fd07bf
Emmssz <150143962+Emmssz@users.noreply.github.com>
2024-05-01 15:30:19 +0200

    changed things

--- Program.cs
+++ Program.cs
@@ -6,106 +6,72 @@ namespace TestCodeAlgoritmer4
     {
         static void Main(string[] args)
         {
-
-
-
             // LÄNKAD LISTA
-
-
             Console.WriteLine("HashTable using LinkedList:");
 
-            
             IHashTableInterface linkedListHashTable = new HashTableLinkedList(10);
 
-            
-            linkedListHashTable.Add("ID1", new Student("Hanna", "20", "h22HaSe"));
-            linkedListHashTable.Add("ID2", new Student("Gustav", "33", "h55GuEr"));
+            linkedListHashTable.Add("student1", new Student("Hanna", "20", "h22HaSe"));
+            linkedListHashTable.Add("student2", new Student("Gustav", "33", "h55GuEr"));
 
-            
-            Console.WriteLine(linkedListHashTable.Get("ID1")); 
-            Console.WriteLine(linkedListHashTable.Get("ID2")); 
+            Console.WriteLine(linkedListHashTable.Get("student1"));
+            Console.WriteLine(linkedListHashTable.Get("student2"));
 
-           
-            linkedListHashTable.Remove("ID1");
+            linkedListHashTable.Remove("student1");
 
             try
             {
-                
-                Console.WriteLine(linkedListHashTable.Get("ID1"));
-            }
-            catch (KeyNotFoundException ex)
-            {
-                Console.WriteLine(ex.Message); 
-            }
-
-            
-            
-            
-            
-            
-            // ARRAY
-            
-            
-            Console.WriteLine("\nHashTable using Array:");
-
-            
-
-            IHashTableInterface arrayHashTable = new HashTableArray(10);
-
-
-            linkedListHashTable.Add("ID1", new Student("Hanna", "20", "h22HaSe"));
-            linkedListHashTable.Add("ID2", new Student("Gustav", "33", "h55GuEr"));
-
-            
-            Console.WriteLine(arrayHashTable.Get("ID1")); 
-            Console.WriteLine(arrayHashTable.Get("ID2")); 
-
-
-            /
-            arrayHashTable.Remove("ID1");
-
-            try
-            {
-                
-                Console.WriteLine(arrayHashTable.Get("ID1"));
+                Console.WriteLine(linkedListHashTable.Get("student1"));
             }
             catch (KeyNotFoundException ex)
             {
                 Console.WriteLine(ex.Message);
             }
 
+            // ARRAY
+            Console.WriteLine("\nHashTable using Array:");
 
-              
-            
-            
-            //  LISTA
-            
-            Console.WriteLine("\nHashTable using List:")
+            IHashTableInterface arrayHashTable = new HashTableArray(10);
 
+            arrayHashTable.Add("student1", new Student("Hanna", "20", "h22HaSe"));
+            arrayHashTable.Add("student2", new Student("Gustav", "33", "h55GuEr"));
 
-            IHashTableInterface listHashTable = new HashTableList(10);
+            Console.WriteLine(arrayHashTable.Get("student1"));
+            Console.WriteLine(arrayHashTable.Get("student2"));
 
-            
-            linkedListHashTable.Add("ID1", new Student("Hanna", "20", "h22HaSe"));
-            linkedListHashTable.Add("ID2", new Student("Gustav", "33", "h55GuEr"));
-
-
-            Console.WriteLine(listHashTable.Get("ID1")); 
-
-            
-            listHashTable.Remove("ID1");
+            arrayHashTable.Remove("student1");
 
             try
             {
-                
-                Console.WriteLine(listHashTable.Get("ID1"));
+                Console.WriteLine(arrayHashTable.Get("student1"));
             }
             catch (KeyNotFoundException ex)
             {
-                Console.WriteLine(ex.Message); 
+                Console.WriteLine(ex.Message);
             }
 
-            Console.ReadLine(); 
+            // LISTA
+            Console.WriteLine("\nHashTable using List:");
+
+            IHashTableInterface listHashTable = new HashTableList(10);
+
+            listHashTable.Add("student1", new Student("Hanna", "20", "h22HaSe"));
+            listHashTable.Add("student2", new Student("Gustav", "33", "h55GuEr"));
+
+            Console.WriteLine(listHashTable.Get("student1"));
+
+            listHashTable.Remove("student1");
+
+            try
+            {
+                Console.WriteLine(listHashTable.Get("student1"));
+            }
+            catch (KeyNotFoundException ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+
+            Console.ReadLine();
         }
     }
 }
